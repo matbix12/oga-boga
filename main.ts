f861058ed5071f01ffb741c7954e938cab018e84
matbix12 <122277660+matbix12@users.noreply.github.com>
2023-01-09 13:55:31 +0100

    Oppdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,62 @@
 radio.onReceivedNumber(function (receivedNumber) {
     bombe = receivedNumber
 })
-input.onGesture(Gesture.Shake, function () {
-    if (bombe > 0) {
-        radio.sendNumber(bombe)
-        bombe = -1
+input.onButtonPressed(Button.A, function () {
+    if (hjem_sjerm == 1) {
+        if (bombe > 0) {
+            radio.setGroup(60)
+            radio.sendNumber(bombe)
+            bombe = -1
+            radio.setGroup(80)
+        }
+    } else {
+        player += -1
+        if (player == 0) {
+            player = 10
+        }
     }
 })
 input.onButtonPressed(Button.AB, function () {
-    bombe = randint(10, 24)
+    if (hjem_sjerm == 1) {
+        radio.setGroup(60)
+        bombe = randint(10, 30)
+        radio.setGroup(80)
+    } else {
+        if (player == 1) {
+        	
+        } else if (player == 1) {
+        	
+        } else if (false) {
+        	
+        } else if (false) {
+        	
+        } else if (false) {
+        	
+        } else if (false) {
+        	
+        } else if (false) {
+        	
+        } else if (false) {
+        	
+        } else if (false) {
+        	
+        } else {
+        	
+        }
+    }
 })
+input.onButtonPressed(Button.B, function () {
+    player += 1
+    if (player == 11) {
+        player = 1
+    }
+})
+let player = 0
 let bombe = 0
+let hjem_sjerm = 0
+hjem_sjerm = 0
 bombe = -1
-radio.setGroup(128)
+radio.setGroup(1)
 basic.forever(function () {
     if (bombe < 0) {
         basic.clearScreen()
